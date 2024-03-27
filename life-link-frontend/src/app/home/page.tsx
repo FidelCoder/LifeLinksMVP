4b1113b2e0d3575176d7cafc681ed1f501e76525
anthonykimani <kimaniantoni@gmail.com>
2024-03-27 10:53:19 +0300

    Feat: Add Dashboard with CatchupTweet

--- life-link-frontend/src/app/home/page.tsx
+++ life-link-frontend/src/app/home/page.tsx
@@ -4,12 +4,14 @@ import React from "react";
 import AppBar from "./components/AppBar";
 import NavBar from "@/components/Navbar";
 import CatchUpStory from "@/components/stories/CatchUpStory";
+import CatchupTweet from "./components/CatchupTweet";
 
 const Home = () => {
   return (
-    <div>
+    <div className="overflow-auto">
       <AppBar />
       <CatchUpStory />
+      <CatchupTweet />
       <NavBar />
     </div>
   );
